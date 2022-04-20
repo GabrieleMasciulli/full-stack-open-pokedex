131a3ef2a5a71b570aace2cfff221e9094b38195
Gabriele Masciulli <gmasciu@gmail.com>
2022-04-20 15:55:04 +0200

    Created e2e test to make sure pokemon page can be navigated to properly from homepage

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -1,9 +1,18 @@
 describe('Pokedex', function () {
+  beforeEach(function () {
+    cy.visit('/')
+  })
+
   it('front page can be opened', function () {
-    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
+
+  it('pokemon page can be navigated to', function () {
+    cy.contains('snorlax').click()
+    cy.contains('gluttony')
+    cy.contains('immunity')
   })
 })
